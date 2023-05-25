a3ca053afc4742c12879359048579a9ef730dfaf
Aakash Sharma <akash.sharma@iesoftek.com>
2023-05-26 01:46:57 +0530

    Updates

---
+++ precache-manifest.3cc33395ff48be92f223a84b118fb87c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634d985cc95de8f69cd41b40dbfca695",
+    "url": "/CodePen/index.html"
+  },
+  {
+    "revision": "2d6775e5c49932083ee2",
+    "url": "/CodePen/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "1e51bb7e9cf3e281b02b",
+    "url": "/CodePen/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "2d6775e5c49932083ee2",
+    "url": "/CodePen/static/js/2.9098bdbf.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/CodePen/static/js/2.9098bdbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e51bb7e9cf3e281b02b",
+    "url": "/CodePen/static/js/main.619addcd.chunk.js"
+  },
+  {
+    "revision": "b6dafa01feb2e4ce0560",
+    "url": "/CodePen/static/js/runtime-main.8110fd6d.js"
+  }
+]);
